89e49d17811a4978d400e2667bd30aec09abedfc
Alexandre F <alexandrefonsecaffaria@gmail.com>
2022-01-04 12:29:08 -0300

    Separando a classe

--- code-of-nature/random-walker/random-walker.js
+++ code-of-nature/random-walker/random-walker.js
@@ -1,16 +1,3 @@
-class Walker {
-    constructor() {
-      this.x = 400 / 2;
-      this.y = 400 / 2;
-    }
-    walk() {
-      stroke(random(20,195));
-      point(this.x, this.y);
-      this.x += random(-1, 1);
-      this.y += random(-1, 1);
-    }
-  }
-  
   let andador;
   
   function setup() {
